36759c6819733cd0e949f3637cf7425bce83a3f7
Wen Zhang <slin30@gmail.com>
2017-01-23 20:25:28 -0500

    Additional progress on parsing piecemeal

--- Scratch/Scratch_TestScript.R
+++ Scratch/Scratch_TestScript.R
@@ -41,50 +41,17 @@ full_multi <- Get_Segments(filters = list(name = "aarti"),
                            accessLevel = "all")
 
 ####
-
-#Helper FUN to make DF from lst or df (L1 or L2)
-.h_lst_to_dt.l1 <- function(x, rn = "field", hierarchy) {
-  if(missing(rn)) {
-    stop("rn is missing")
-  }
-  bind <- do.call(cbind, x) # handle list elements that are vectors > 1L
-  # TODO: Check that all vectors are same length
-  dt <- as.data.table(t(bind), keep.rownames = rn)
-  out <- melt(dt, id.vars = rn, variable.name = "idx", variable.factor = FALSE)
-  out[, "idx" := as.integer(stringr::str_extract(get("idx"), "\\d+"))]
-  out[, "hierarchy" := hierarchy]
-}
-
-.h_lst_to_dt.l2 <- function(x, type, autoID = FALSE, hierarchy) {
-  if(missing(type)) {
-    stop("rn and/or type  missing")
-  }
-  bind <- do.call(cbind, x) # handle list elements that are vectors > 1L
-  # TODO: Check that all vectors are same length
-  dt <- as.data.table(t(bind), keep.rownames = "subfield")
-  out <- melt(dt, id.vars = "subfield", variable.name = "subidx", variable.factor = FALSE)
-  out[, "field" := type]
-  out[, "hierarchy" := as.integer(hierarchy)]
-  
-  if(autoID) {
-    out[, "idx" := as.integer(stringr::str_extract(get("subidx"), "\\d+"))]
-  }
-  
-  return(out)
-}
-
-
-recur_get_names <- function(x, i = 0L, nm =NULL) {
-  
-  if(!is.list(x)) {
-    nm <- c(names(x), nm)
-    return(nm)
-  } else {
-    print(i)
-    recur_get_names(x = unlist(x, recursive = FALSE), i = i+1, nm = c(nm))
-  }
-  
-}
+# recur_get_names <- function(x, i = 0L, nm =NULL) {
+#   
+#   if(!is.list(x)) {
+#     nm <- c(names(x), nm)
+#     return(nm)
+#   } else {
+#     print(i)
+#     recur_get_names(x = unlist(x, recursive = FALSE), i = i+1, nm = c(nm))
+#   }
+#   
+# }
 
 
 .parse_container <- function(x) {
@@ -119,26 +86,37 @@ recur_get_names <- function(x, i = 0L, nm =NULL) {
   }
     # However, if someone passed in rules , need to nest it a level as df
   if(all(c("type", "operator", "rules") %in% names(x))) {
-    cont <- data.frame(container = x)
+    cont <- data.frame(container = seq_len(nrow(x)))
+    cont$container <- data.frame(type = x[["type"]])
+    
+    x_class <- vapply(x, class, FUN.VALUE = character(1))
+    x_flat <- x[, which(x_class != "list")]
+    
+    cont$container <- x_flat
+    cont$container$rules <- x[["rules"]]
     return(cont)
   }
   
 }
 
-a <- full_multi[["definition"]]
-b <- a$container
+# a <- full_multi[["definition"]]
+# b <- a$container
+# 
+# 
+# p1 <- .parse_container(full_multi)
+# p2 <- .parse_container(a)
+# p3 <- .parse_container(b)
 
-
-.parse_container(full_multi)
-.parse_container(a)
-.parse_container(b)
+# need parsers for shares, compatibility, and tags
 
 
 
 # Testing -----------------------------------------------------------------
+all_mg <- GS_ALL(filters = list(name = "KNOVEL Paying"))
 
 
-x <- full_multi
+#x <- full_multi
+x <- all_mg
 
 x_filt <- lapply(x, class)
 
@@ -147,31 +125,45 @@ x_vec       <- x[x_filt != "data.frame"]
 x_container <- as.list(x[[c("definition", "container")]])
 
 # handle x_vec, will always have values unless error
-dt.x_vec <- .h_lst_to_dt.l1(x_vec, rn = "field", hierarchy = 1L)
+#dt.x_vec <- .h_lst_to_dt.l1(x_vec, rn = "field", hierarchy = 1L)
+dt.x_vec <- do.call(cbind, x_vec) %>% t %>% as.data.table(., keep.rownames = "field")
+
+out_dt.x_vec <- melt(dt.x_vec, id.vars = "field", variable.factor = FALSE)
+out_dt.x_vec[, value := fix_blank(value)]
 
 # Now handle things that need to be unnested
 
 # container needs to be additionally separated
-x_container_only <- x_container[!names(x_container) == "rules"]
-dt.x_container_only <- .h_lst_to_dt.l2(x_container_only, type = "container", autoID = TRUE, hierarchy = 2L)
-dt.x_container_only[, subidx:= NULL]
+#x_container_only <- x_container[!names(x_container) == "rules"]
+x_container_only <- .parse_container(x)
+x_container_only <- do.call(cbind, x_container_only$container[, c("type", "operator")]) %>%
+  t %>% as.data.table(., keep.rownames = "subfield")
+# append field, which is container
+x_container_only$field <- "container"
+out_x_container_only <- melt(x_container_only, id.vars = c("field", "subfield"), variable.factor = FALSE)
+out_x_container_only[, value := fix_blank(value)]
 
-
+# Now handle rules
 x_rules <- x_container[[c("rules")]] %>%
   Map(as.data.table, .)
+x_rules <- lapply(x_rules, function(f) f[, "field" := "container"])
+x_rules <- lapply(x_rules, function(f) f[, "subfield" := "rules"])
+x_rules_melt <- lapply(x_rules, function(f) melt(f, id.vars = c("field", "subfield"), variable.name = "subfield_name", variable.factor = FALSE))
+x_rules_melt <- rbindlist(x_rules_melt, use.names = TRUE, idcol = "variable")
+x_rules_melt[, variable := paste0("V", variable)]
 
+# Outputs
+out <- rbindlist(list(out_dt.x_vec, out_x_container_only, x_rules_melt), use.names = TRUE, fill = TRUE) %>%
+  setnames(., "variable", "idx") %>%
+  .[, value := fix_blank(value)] %>%
+  .[!is.na(value)] %>%
+  melt(., id.vars = c("field", "subfield", "subfield_name", "idx"), measure.vars = list("value")) %>%
+  .[, variable := NULL]
 
-dt.x_rules <- lapply(x_rules, function(f) .h_lst_to_dt.l2(f, type = "rule", autoID = FALSE, hierarchy = 3L)) %>%
-  rbindlist(., use.names = TRUE, idcol = "idx")
+out_cast <- dcast(out, ... ~ idx, value.var = "value", fun.aggregate = function(x) paste(unique(x), collapse = ", "))
 
-
-out <- rbindlist(list(dt.x_vec, dt.x_container_only, dt.x_rules), use.names = TRUE, fill = TRUE, idcol = FALSE)
-out[, "value" := fix_blank(get("value"))]
-
-setcolorder(out, c("hierarchy", "field", "idx", "subfield", "subidx", "value"))
-
-out_cast <- dcast(out, hierarchy + field + subfield + subidx~ idx, value.var = "value")
-setorder(out_cast, hierarchy, subidx)
+remelt <- melt(out_cast, measure.vars = patterns("V\\d+"), na.rm = TRUE) # this should be the melted output
+recast <- dcast(remelt, ... ~ variable, value.var = "value") # just a check
 ####
 
 
@@ -181,13 +173,5 @@ setorder(out_cast, hierarchy, subidx)
 # Testing output space ----------------------------------------------------
 
 
-lapply(full_multi, ulnames) %>% unlist(., use.names = TRUE)
-ulnames(full_multi) %>% print
-ulnames(full_multi$definition)
 
-
-all_single <- GS_ALL(filters = list(name = "aarti"), 
-                     accessLevel = "all")
-
-all_mg <- GS_ALL(filters = list(name = "KNOVEL Paying"))
 
