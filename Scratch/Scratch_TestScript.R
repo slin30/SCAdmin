92d99203bda9540b9136c619eeca25a685d1b3b1
Wen Zhang <slin30@gmail.com>
2017-02-04 23:32:16 -0500

    Progress towards nested container-rules parsing

--- Scratch/Scratch_TestScript.R
+++ Scratch/Scratch_TestScript.R
@@ -79,7 +79,9 @@ call.Get_Segments(sort = "A")
 call.Get_Segments(filters = list(name = "A", alt="B"))
 
 
-# Working -----------------------------------------------------------------
+
+# Initial calls for next step testing -------------------------------------
+
 
 full_single <- easy.Get_Segments(filters = list(name = "aarti"), 
                                  fields = c("definition", "description", 
@@ -103,79 +105,15 @@ WZ_out <- merge(WZ_all$segment_meta, WZ_all$defn, by = c("segment_id"))
 WZ_call <- GS_ALL()
 WZ_call_split <- .split_segment_ret(WZ_call)
 
-type_1 <- WZ_call_split$s300000520_57e0327ae4b007430cbdcdc0 # this one part of the stacked segment. Not created properly
+# this one part of the stacked segment. Not created properly
 # since is unnecessarily nested, but perhaps a good test case
-type_2 <- WZ_call_split$s300000520_57e0343be4b007430cbdcdc3 # this is the stacked segment
-type_3 <- WZ_call_split$s300000520_582ca0d2e4b0a4d9dc2936ac # this is a pretty terribly created (TLV) nested
+type_1 <- WZ_call_split$s300000520_57e0327ae4b007430cbdcdc0 
+# this is the stacked segment
+type_2 <- WZ_call_split$s300000520_57e0343be4b007430cbdcdc3 
+# this is a pretty terribly created (TLV) nested
 # unnecessarily as well.
+type_3 <- WZ_call_split$s300000520_582ca0d2e4b0a4d9dc2936ac 
 
-# Dirty return parsing ----------------------------------------------------
-
-# This currently works for nested elements, but not stacked segments, 
-#  and need to figure out the logic for > 1 segment in a single call where one or more
-#  is nested (but NOT stacked)
-# Which means I need to identify stacked segments and filter them out
-extract_nested <- function(x, d = 0L, out = vector("list", 0L)) {
-  # keep it simple for testing for now
-  # assume you get the right input
-  message("In iter ", d, " input form of x is", str(x))
-  
-  # check if container is present
-  if("definition" %in% names(x)) {
-    x <- x[["definition"]][["container"]][["rules"]][[1]]
-    out[[paste0("iter_", d)]] <- list(x)
-    d <- d+1L
-  }
-  
-  if("container" %in% names(x)) {
-    x <- x[["container"]]
-  }
-  
-  if(is.data.frame(x)) {
-    x <- as.list(x)
-  }
-
-  message("In iter ", d, " intermediate form of x is", str(x))
-  
-  
-  chk_lst <- is.list(x)
-  chk_nm <- "rules" %in% names(x)
-  
-  if(chk_lst & chk_nm) {
-    x <- Filter(function(x) !is.null(x), x[["rules"]])
-  }
-  
-  message("In iter ", d, " Filtered structure of x is", str(x))
-  
-  if(is.null(x)) {
-    return(out)
-  }
-  
-  chk_L1 <- is.data.frame(x[[1]])
-  chk_L1_nm <- "container" %in% names(x[[1]])
-  
-  # if(all(chk_lst, chk_nm, chk_L1, chk_L1_nm) && !is.null(x)) {
-  if(all(chk_L1, chk_L1_nm) && !is.null(x)) {
-    
-    out[[paste0("iter_", d)]] <- x
-    # out <- lapply(out, function(f) Filter(function(x) !is.null(x), f))
-    # extract_nested(x = x[[1]][["container"]], d = d + 1L, out)
-    extract_nested(x = x[[1]][["container"]], d = d + 1L, out)
-  } else { # we are at the lowest level; still needs to tweaking 
-    x <- x
-    if(length(x) > 1L) {
-      x <- do.call(rbind, x)
-    }
-    new_nm <- paste0("iter_", d+1L)
-    if(is.list(x) && ! is.data.frame(x)) {
-      out[[new_nm]] <- x
-    } else {
-      out[[new_nm]] <- list(x)
-    }
-    return(out)
-  }
-  
-}
 
 # Make segments testing ---------------------------------------------------
 
@@ -208,6 +146,110 @@ seg_rules <- make_element_rules(element = mg_dt$defn$element,
 seg_container <- make_segment_container(type = "hits", operator = "or", exclude = TRUE, rules = seg_rules)
 
 seg_body <- make_segment_body(segment_container = seg_container, segment_meta = seg_meta)
+
+
+
+
+# More nested container parsing -------------------------------------------
+
+## With a bit more insight into how nested containers can be built, properly, take another
+## stab at parsing them
+
+
+# Here are some properly built nested segments, as a first test case:
+seg_test <- call.Get_Segments(accessLevel = "all", filters = list(name = "Databases all"), 
+                               fields = c("tags", "shares",
+                                          "description", "owner",
+                                          "modified", "compatibility",
+                                          "favorite", "reportSuiteID",
+                                          "definition")
+                               )
+
+# Let's grab only a sinle segment
+test_split <- .split_segment_ret(seg_test)
+single_test <- test_split[[2]]
+
+## Reference set-- an unnested segment for comparison
+seg_ref <- call.Get_Segments(accessLevel = "owned", filters = list(name = "ThermoPhysProd"), 
+                              fields = c("tags", "shares",
+                                         "description", "owner",
+                                         "modified", "compatibility",
+                                         "favorite", "reportSuiteID",
+                                         "definition")
+)
+
+ref_split <- .split_segment_ret(seg_ref)
+single_ref <- ref_split[[1]]
+
+### Focus on rules
+
+# first, simply split into definition and not definition
+split_seg_return <- function(x) {
+  if(!"definition" %in% names(x)) {
+    stop("definition missing")
+  }
+  
+  seg_meta <- setdiff(names(x), "definition")
+  seg_def <- c("definition")
+  
+  
+  
+  list(segment_meta = x[seg_meta], 
+       segment_def = x[[seg_def]]
+       )
+}
+
+p1.ref <- split_seg_return(single_ref)
+p1.tst <- split_seg_return(single_test)
+
+
+
+# Then, check names to see if this is nested
+## TODO: Write a parser that properly names output for inherently non-nested containers
+## and pass such structures to this function; the parse_nested_container should only
+## be used for actual nested containers, as the naming convention is recursive and 
+## therefore confusing for non-recursive inputs
+
+## Remember, the recursive logic will flip the order of the if...else 
+## for things that are natively recursive, vs. not. Hence the naming confusion. 
+
+## NB: This is also noted in comments after this function. 
+
+# for now, input the "segment_def" part of split_set_ret
+parse_nested_container <- function(x, lst = c()) {
+  
+  is_nested <- "container" %in% names(x[[c("container", "rules")]][[1]])
+  
+
+  if(!is_nested) {
+    x.tmp <- x[["container"]]
+    lst <- c(lst, 
+             list(sub_cont_meta = x.tmp[setdiff(names(x.tmp), "rules")],
+                  sub_cont_rule = x.tmp[["rules"]]
+             )
+    )
+    return(lst)
+  } else {
+    x.tmp      <- x[["container"]]
+    x.tmp_nest <- x.tmp[["rules"]][[1]]
+    
+    lst <- c(lst, 
+             list(cont_meta = x.tmp[setdiff(names(x.tmp), "rules")]
+                  #cont_rule = x.tmp_nest # I think this is actually redundant for natively recursive inputs...
+             )
+    )
+    parse_nested_container(x = x.tmp_nest, lst = lst)
+    
+  }
+  
+}
+
+parse_p1.ref <- parse_nested_container(p1.ref$segment_def) # should use a different parser for containers that are
+# intrinsically not nested, because naming for recursive structures is confusing is not natively recursive, in 
+# the current function. Or could fix the function naming logic...
+
+parse_p1.tst <- parse_nested_container(p1.tst$segment_def) # this is closer to what you want
+
 
 
 
